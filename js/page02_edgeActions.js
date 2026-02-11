011afb772322f483d752afc7ac9633dd14c00d48
Sanil-Surve <sanilsurve42@gmail.com>
2026-02-11 19:21:02 +0530

    feat: Add initial Adobe Edge Animate project files for a multi-page interactive experience, including HTML, JavaScript, and image assets.

---
+++ js/page02_edgeActions.js
@@ -0,0 +1,50 @@
+/***********************
+* Adobe Edge Animate Composition Actions
+*
+* Edit this file with caution, being careful to preserve 
+* function signatures and comments starting with 'Edge' to maintain the 
+* ability to interact with these actions from within Adobe Edge Animate
+*
+***********************/
+(function($, Edge, compId){
+var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
+
+   //Edge symbol: 'stage'
+   (function(symbolName) {
+      
+      
+      Symbol.bindElementAction(compId, symbolName, "${Stage}", "swipeleft", function(sym, e) {
+        /* window.open("page04.html","_self")*/
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${page1-asset1}", "click", function(sym, e) {
+         window.open("page04.html","_self")
+         
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${Stage}", "swiperight", function(sym, e) {
+         /*window.open("page01.html","_self")*/
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${page2-asset2}", "click", function(sym, e) {
+         window.open("page01.html","_self")
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${page3-asset2}", "click", function(sym, e) {
+         window.open("page03.html","_self")
+
+      });
+      //Edge binding end
+
+   })("stage");
+   //Edge symbol end:'stage'
+
+})(window.jQuery || AdobeEdge.$, AdobeEdge, "EDGE-768282051");
---
+++ js/page06_edge.js
@@ -0,0 +1,71 @@
+/*jslint */
+/*global AdobeEdge: false, window: false, document: false, console:false, alert: false */
+(function (compId) {
+
+    "use strict";
+    var im='../images/',
+        aud='../media/',
+        vid='../media/',
+        js='../js/',
+        fonts = {
+        },
+        opts = {
+            'gAudioPreloadPreference': 'auto',
+            'gVideoPreloadPreference': 'auto'
+        },
+        resources = [
+        ],
+        scripts = [
+        ],
+        symbols = {
+            "stage": {
+                version: "5.0.1",
+                minimumCompatibleVersion: "5.0.0",
+                build: "5.0.1.386",
+                scaleToFit: "both",
+                centerStage: "both",
+                resizeInstances: false,
+                content: {
+                    dom: [
+                        {
+                            id: 'page7-asset1',
+                            type: 'image',
+                            rect: ['0px', '0px', '1080px', '720px', 'auto', 'auto'],
+                            fill: ["rgba(0,0,0,0)",im+"page7-asset1.png",'0px','0px']
+                        },
+                        {
+                            id: 'page1-asset1',
+                            type: 'image',
+                            rect: ['1012px', '330px', '21px', '44px', 'auto', 'auto'],
+                            fill: ["rgba(0,0,0,0)",im+"page1-asset1.png",'0px','0px']
+                        },
+                        {
+                            id: 'page4-asset1',
+                            type: 'image',
+                            rect: ['969px', '47px', '64px', '64px', 'auto', 'auto'],
+                            fill: ["rgba(0,0,0,0)",im+"page4-asset1.png",'0px','0px']
+                        }
+                    ],
+                    style: {
+                        '${Stage}': {
+                            isStage: true,
+                            rect: ['null', 'null', '1080px', '720px', 'auto', 'auto'],
+                            overflow: 'hidden',
+                            fill: ["rgba(255,255,255,1)"]
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 0,
+                    autoPlay: true,
+                    data: [
+
+                    ]
+                }
+            }
+        };
+
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+
+    if (!window.edge_authoring_mode) AdobeEdge.getComposition(compId).load("page06_edgeActions.js");
+})("EDGE-768282051");
